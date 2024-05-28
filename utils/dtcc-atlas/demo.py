11a7466b1b8ad9dc4ae9924de3ce6eaa4e873783
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2024-05-28 10:39:48 +0200

    Added initial demo for server and client sides.

---
+++ utils/dtcc-atlas/demo.py
@@ -0,0 +1,21 @@
+# Download the demo on server side
+
+# Define stored input data, e.g. Tile_01,Tile_02 directories with .gpgk file in each
+
+# Read those two Tiles and create an catalog.json
+
+# Download the demo on client side
+
+# Initialize client side (I guess we need a local catalog.json?)
+
+# Add parameters (i.e. where is the server located, hostname, port)
+
+# Request data for a specific bounding box
+
+# Get the data from server
+
+# Request data for a specific bounding box that client only has partial data
+
+# Get the missing data from server
+
+
