7707ba7aec588c3f2b672ec5934e897b34c9623b
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2025-01-07 11:46:20 +0200

    Exposed download function.

--- src/dtcc_template/__init__.py
+++ src/dtcc_template/__init__.py
@@ -4,5 +4,6 @@ from ._bindings import add, sub, Complex
 # Import Python code
 from dtcc_template.multiplication import mul
 from dtcc_template.division import div
+from dtcc_template.client import download_lidar
 
-__all__ = ["add", "sub", "mul", "div", "Complex"]
+__all__ = ["add", "sub", "mul", "div", "Complex", "download_lidar"]
