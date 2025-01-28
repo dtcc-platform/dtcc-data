8a30adefafeebbddef37fa62519a9fbe7e21c489
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2025-01-28 12:08:38 +0200

    Updated __init__.py to expose both functions from Overpass

--- src/dtcc_data/__init__.py
+++ src/dtcc_data/__init__.py
@@ -2,6 +2,6 @@
 from dtcc_data.multiplication import mul
 from dtcc_data.division import div
 from dtcc_data.lidar import download_lidar
-from dtcc_data.overpass import get_roads_for_bbox, get_roads_for_bbox
+from dtcc_data.overpass import get_roads_for_bbox, get_buildings_for_bbox
 
 __all__ = ["mul", "div", "download_lidar", "get_roads_for_bbox", "get_roads_for_bbox"]
