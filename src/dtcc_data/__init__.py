ac34021af869a964717bd9ce4541adddde575442
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2025-01-28 11:52:17 +0200

    Updated __init__.py with overpass

--- src/dtcc_data/__init__.py
+++ src/dtcc_data/__init__.py
@@ -2,5 +2,6 @@
 from dtcc_data.multiplication import mul
 from dtcc_data.division import div
 from dtcc_data.client import download_lidar
+from dtcc_data.overpass import get_roads_for_bbox, get_roads_for_bbox
 
 __all__ = ["mul", "div", "download_lidar"]
