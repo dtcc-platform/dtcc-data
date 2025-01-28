d470d6471d1c26bd0bf2834bd0a3971e7fc601c0
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2025-01-28 09:55:46 +0000

    Few fixes and functions exposing.

--- src/dtcc_data/__init__.py
+++ src/dtcc_data/__init__.py
@@ -1,7 +1,7 @@
 # Import Python code
 from dtcc_data.multiplication import mul
 from dtcc_data.division import div
-from dtcc_data.client import download_lidar
+from dtcc_data.lidar import download_lidar
 from dtcc_data.overpass import get_roads_for_bbox, get_roads_for_bbox
 
-__all__ = ["mul", "div", "download_lidar"]
+__all__ = ["mul", "div", "download_lidar", "get_roads_for_bbox", "get_roads_for_bbox"]
