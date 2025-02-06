2a55bcba17a98e1c3a809d96307eca1e82b763e0
Themistoklis Arvanits <themistoklisarv2001@gmail.com>
2025-02-06 13:43:27 +0100

    Fixed naming in __init__

--- src/dtcc_data/__init__.py
+++ src/dtcc_data/__init__.py
@@ -6,4 +6,4 @@
 #from dtcc_data.geopkg import download_tiles
 from dtcc_data.wrapper import download_data, download_pointcloud, download_footprints
 from dtcc_data.cache import empty_cache
-__all__ = ["download_data", "download_lidar", "download_footprints", "empty_cache"]
+__all__ = ["download_data", "download_pointcloud", "download_footprints", "empty_cache"]
