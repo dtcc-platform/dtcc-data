92d716830c5087d649526391a2077214c34c530d
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2025-02-10 04:11:45 +0000

    Deploy fixes.

--- src/deploy/run.sh
+++ src/deploy/run.sh
@@ -1,1 +1,2 @@
 uvicorn server-gpkg-ssh:app --host 0.0.0.0 --port 8001 --workers=4
+uvicorn server-lidar-ssh:app --host 0.0.0.0 --port 8000 --workers=4
--- src/deploy/run2.sh
+++
@@ -1,1 +0,0 @@
-uvicorn server-lidar-ssh:app --host 0.0.0.0 --port 8000
